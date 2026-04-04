789b2c964083d8f0f73645a74f087fda5a95d647
¨dantepetrizzi¨ <dante.petrizzi@gmail.com>
2026-04-04 23:46:57 +0000

    Enviamos el valor de CLAVE al servidor antes de armar y enviar el paquete

--- client/src/client.c
+++ client/src/client.c
@@ -61,7 +61,7 @@ int main(void)
 	// Enviamos al servidor el valor de CLAVE como mensaje
 	
 	enviar_mensaje(clave, conexion);
-	return EXIT_SUCCESS;
+	
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
